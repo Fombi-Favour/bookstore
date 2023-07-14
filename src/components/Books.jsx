8244865d51bcec78e5c16bd6422d7407cca04da0
Fombi-Favour <fombifavour@gmail.com>
2023-07-14 21:02:16 +0100

    Update Books.jsx

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,43 +1,30 @@
 import React from 'react';
-import styles from '../style/Header.module.css';
+import Book from './Book';
+// import styles from '../style/Header.module.css';
 
-const Books = () => (
-  <div className={styles.lesson_panel}>
-    <div className={styles.row_1}>
-      <div>
-        <div className={styles.text}>
-          <span className={styles.category}>Action</span>
-          <span className={styles.title}>The Hunger Games</span>
-          <span className={styles.author}>Suzanne Collins</span>
-        </div>
-        <ul className={styles.action}>
-          <li>
-            <button type="button">
-              Comments
-            </button>
-          </li>
-          <li>
-            <button type="button">
-              Remove
-            </button>
-          </li>
-          <li>
-            <button type="button">
-              Edit
-            </button>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.progress}>
-        <p />
-        <div className={styles.progress_text}>
-          <span>64%</span>
-          <span>Completed</span>
-        </div>
-      </div>
-    </div>
-    <div className={styles.row_2}>CURRENT CHAPTER</div>
-  </div>
-);
+const Books = () => {
+  const books = [
+    {
+      id: 1,
+      title: 'Romeo and Julliet',
+      author: 'William Shakespare',
+      category: 'Romance',
+    },
+    {
+      id: 2,
+      title: 'The Hunger Games',
+      author: 'Suzanne Collins',
+      category: 'Action',
+    },
+  ];
+
+  return (
+    <ul>
+      {books.map((book) => (
+        <Book key={book.title} title={book.title} author={book.author} category={book.category} />
+      ))}
+    </ul>
+  );
+};
 
 export default Books;
