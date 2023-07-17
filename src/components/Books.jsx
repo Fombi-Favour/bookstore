61d3a9b6f8778920edc23038c52d37b6b957b959
Fombi-Favour <fombifavour@gmail.com>
2023-07-17 22:59:39 +0100

    Update Books.jsx

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,27 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 
 const Books = () => {
-  const books = [
-    {
-      id: 1,
-      title: 'Romeo and Julliet',
-      author: 'William Shakespare',
-      category: 'Romance',
-    },
-    {
-      id: 2,
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-      category: 'Action',
-    },
-  ];
+  // const books = [
+  //   {
+  //     id: 1,
+  //     title: 'Romeo and Julliet',
+  //     author: 'William Shakespare',
+  //     category: 'Romance',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'The Hunger Games',
+  //     author: 'Suzanne Collins',
+  //     category: 'Action',
+  //   },
+  // ];
+
+  const { bookItems } = useSelector((store) => store.books);
 
   return (
     <ul>
-      {books.map((book) => (
-        <Book key={book.title} title={book.title} author={book.author} category={book.category} />
-      ))}
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      {bookItems.map((item) => <Book key={item.item_id} {...item} />)}
     </ul>
   );
 };
