bb3ff53d8cb1812e26782d48df6e6af1312dec91
Fombi-Favour <fombifavour@gmail.com>
2023-07-17 21:31:51 +0100

    Update categoriesSlice.js

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -2,11 +2,19 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   categories: [],
+  status: 'Under construction',
 };
 
 const categoriesSlices = createSlice({
   name: 'catgories',
   initialState,
+  reducers: {
+    statusCheck: (state, action) => {
+      state.categories = action.payload === 'Under construction' ? 'Under construction' : state.categories;
+    },
+  },
 });
+
+export const { statusCheck } = categoriesSlices.actions;
 
 export default categoriesSlices;
