4d288a33b2e5de0058b5df555f9e97784b1d34f9
Fombi-Favour <fombifavour@gmail.com>
2023-07-19 13:47:32 +0100

    Update AddBooks.jsx

--- src/components/AddBooks.jsx
+++ src/components/AddBooks.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuid } from 'uuid';
 import { postBookItems } from '../redux/books/booksSlice';
+import styles from '../style/Header.module.css';
 
 const AddBooks = () => {
   const [title, setTitle] = useState('');
@@ -25,18 +26,35 @@ const AddBooks = () => {
   };
 
   return (
-    <div>
+    <div className={styles.add_form}>
       <h2>ADD NEW BOOK</h2>
-      <div>
-        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Book Title" />
-        <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} placeholder="Author" />
-        <select value={category} onChange={(e) => setCategory(e.target.value)}>
-          <option value="other">Category</option>
-          <option value="action">Action</option>
-          <option value="fiction">Fiction</option>
-          <option value="nonfiction">Nonfiction</option>
+      <div className={styles.form}>
+        <input
+          type="text"
+          value={title}
+          className={styles.input_title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Book title"
+        />
+        <input
+          type="text"
+          value={author}
+          className={styles.input_author}
+          onChange={(e) => setAuthor(e.target.value)}
+          placeholder="Author"
+        />
+        <select
+          value={category}
+          className={styles.input_category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="Action">Action</option>
+          <option value="Fiction">Fiction</option>
+          <option value="Nonfiction">Nonfiction</option>
         </select>
-        <button type="button" onClick={addNew}>ADD BOOK</button>
+        <button type="button" onClick={addNew} className={styles.btn_submit}>
+          ADD BOOK
+        </button>
       </div>
     </div>
   );
