276a441677d023cee699e311390095d46d205eb7
Fombi-Favour <fombifavour@gmail.com>
2023-07-19 13:47:42 +0100

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Books from '../components/Books';
 import AddBooks from '../components/AddBooks';
+import '../index.css';
 
 const Home = () => (
   <div>
     <Books />
+    <hr />
     <AddBooks />
   </div>
 );
